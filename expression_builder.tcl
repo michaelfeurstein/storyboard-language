667973c34445a0e58fb47dc41a3a21e731b623dd
Michael Feurstein <michael.feurstein@wu.ac.at>
2021-01-27 16:35:18 +0100

    started matchList method to compare class create parameters with dict

--- expression_builder.tcl
+++ expression_builder.tcl
@@ -18,8 +18,28 @@ nx::Class create StoryboardBuilder {
 			puts "matched a video class"
 			# mapping is hard coded - TODO - make this more dynamic 
 			# the order of elements in the stack will not always be this way - account for that
-			# CONTINUE HERE: maybe regsub through the stack
-			puts stack:${:stack}
+			# maybe regsub through the stack
+			# go thorugh stack and match corresponding parameters
+			# stacklength should be even as paired key:value elements
+			# unsure what to do i uneven ... is there a case?
+			# parse through keys and match with class parameters
+			# if key and class parameter match set it up
+			puts stacklength[llength ${:stack}]:${:stack}
+			puts dict:[dict keys ${:stack}]
+			# folgendes klappt nur wenn ich keine required parameter habe
+			#puts [info class constructor nx::$class]
+		  	#set configInfo [[$class new] info lookup syntax configure]
+		  	set configInfo [$class info lookup syntax create]
+			puts configInfo:$configInfo	
+			puts matchLists:[:matchLists $configInfo [dict keys ${:stack}]]
+			
+			#foreach x $configInfo {
+			  # hier dann die parameter matchen mit dem stac
+			  # setzt voraus dass ich sie richtig setze
+			#  puts x:[string trim $x "-?"]
+#
+#			}
+
 			lassign ${:stack} a videoID b videoLink c title d length
 			set :stack [$class new -childof [self] -$b $title -videoSource $videoLink -length $length]
 		  }
@@ -34,7 +54,23 @@ nx::Class create StoryboardBuilder {
 			puts "matched nothing"
 		  }
 		 }
-	} 
+	}
+
+	# CONTINUE HERE: not there yet
+	# compare create parameters with dict 
+	# if they match, use them
+	:method matchLists {a b} {
+		set r {}
+		foreach i $a {
+		  	set e [string trim $i "?-"]
+			puts "match:e:$e b:$b i:$i"
+			if { [lsearch -glob $b $e]==1 } {
+			  puts "matched:b:$b with e:$e"	
+			  lappend r $e
+			}
+		}
+		return $r
+	}	  
 
 	# DYNAMIC RECEPTION
 	:method unknown {v args} {
