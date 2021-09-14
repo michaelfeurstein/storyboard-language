e50a42b5048db1f84cc68c16ae3bd330b7778f5e
Michael Feurstein <michael.feurstein@wu.ac.at>
2021-09-14 12:23:59 +0200

    archive backup for march 2021 progress

---
+++ archive/01-draft-mar21/language_model.tcl
@@ -0,0 +1,80 @@
+package req nx
+
+namespace eval StoryBoard {
+
+#
+# ContentFragment
+# A content fragment resembles an atomic element of its own type (video, text, image).
+# Multiple content fragments can be combined into content objects such as a website (text + image).
+# Content objects form Learning Objects.
+#
+
+nx::Class create ContentFragment
+
+nx::Class create Text -superclasses ContentFragment
+nx::Class create Image -superclasses ContentFragment
+
+#
+# Video 
+# A video is of type ContentFragment.
+# A video may have multiple highlights.
+#
+# videoSource (file location or link)
+# length in seconds (starttime=0s, endtime=length) 
+#
+
+nx::Class create Video -superclasses ContentFragment {
+	:property -accessor public {title empty}
+	:property -accessor public {videoSource empty}
+	:property -accessor public {length:integer 0}
+	:property -accessor public {is360Video:boolean false}
+
+	# Every video per default is a highlight
+	:method init {} {
+	  Highlight create [self]::highlight -title ${:title} -starttime 0 -endtime ${:length}
+	  if ${:is360Video} {360Video create [self]::360video}
+	} 
+}
+
+nx::Class create 360Video -superclasses Video {
+	# TODO: 360-degree properties
+}
+
+#
+# Video Highlight (see AuthTask01)
+# A highlight is associated with exactly one video / a video may have multiple highlights
+# Another way to think of highlights is chapters or segments
+# A highlight has a start time and an end time.
+# It needs a textual title in order to provide a clickable link for a GUI.
+# A highlight can be cropped, which means that only the time range is shown in the final result for the viewer
+#
+
+nx::Class create Highlight {
+	:property -accessor public {starttime:integer}
+	:property -accessor public {endtime:integer}
+	:property -accessor public {title}
+	:property -accessor public {crop:boolean false}
+
+	:method init {} {
+	  TimeRange create [self]::timerange -starttime ${:starttime} -endtime ${:endtime}
+	}
+}
+
+nx::Class create TimeRange {
+	:property -accessor public {starttime:integer}
+	:property -accessor public {endtime:integer}
+
+	:method getTimeRange {} {
+	  return [expr {${:endtime} -${:starttime}}]
+	}
+}
+
+nx::Class create Interaction
+
+nx::Class create Annotation -superclasses Interaction {
+	
+}
+
+namespace export ContentFragment Text Image Video Highlight TimeRange
+}
+
---
+++ archive/01-draft-mar21/model_tester.tcl
@@ -0,0 +1,66 @@
+#!/usr/bin/env tclsh
+package require Tcl 8.6
+package require nx
+
+source language_model.tcl
+source parser.tcl
+source expression_builder.tcl
+
+namespace import StoryBoard::*
+
+set storyboardFile ""
+
+if { $argc != 1 } {
+  	puts "model_tester.tcl requires a storyboard as input"
+	puts "For example, tclsh model_tester.tcl storyboard_example_A_01_blank"  
+	exit
+} else {
+	set storyboardFile [lindex $argv 0]
+}
+
+puts storyboardFile:$storyboardFile
+
+# Direct instantiations of model:
+
+# begin
+
+Text new
+Image new
+Video new
+
+Text create testText
+Image create testImage
+
+Video create testVideo -title {Test Video} -videoSource {http://www.youtube.com/12345} -length 12345
+
+# end
+
+puts "--- testVideo"
+puts title:[testVideo title get]
+puts videoSource:[testVideo videoSource get]
+puts length:[testVideo length get]
+
+puts "--- testVideo::highlight"
+puts crop:[testVideo::highlight crop get]
+puts title:[testVideo::highlight title get]
+puts starttime:[testVideo::highlight starttime get]
+puts endtime:[testVideo::highlight endtime get]
+
+#? {llength [ContentFragment info instances -closure]} 6
+puts ContentFragments:[llength [ContentFragment info instances -closure]]
+puts Highlights:[llength [Highlight info instances -closure]]
+
+# Setup Parser
+set internalParser [StoryboardParser new -storyboardFile $storyboardFile]
+
+# Internal DSL (indirect instantiation)
+
+# begin
+
+set internalBuilder [StoryboardBuilder new]
+$internalBuilder from [$internalParser storyboardDict get]
+
+# end
+puts ContentFragments:[llength [ContentFragment info instances -closure]]
+puts Highlights:[llength [Highlight info instances -closure]]
+puts TimeRanges:[llength [TimeRange info instances -closure]] 
