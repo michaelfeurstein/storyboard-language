1df0981b15d299a3a3c2866190ed0f258d3028a4
Michael Feurstein <michael.feurstein@wu.ac.at>
2021-10-06 10:34:33 +0200

    quick cleanup
    -trailing whitespaces
    -spaces before tab

--- language_model.tcl
+++ language_model.tcl
@@ -5,19 +5,19 @@ namespace eval StoryBoard {
 nx::Class create Helper {
 
 	##
-  	## isInstanceAvailable
-  	##	
-  	# input: class (the class of which an instance is to be searched e.g. ::Timestamp or ::Video)
-  	# input: id (the id of the class instance we are looking for e.g.: timestamp7 or video2)
-  	# 
-  	# returns:
-  	#  - in case the instance is found the instance is returned (return $x)
-  	#  - in case there are no matching instances found return code 0 is returned
-  	#    this could also mean that there are instances available but not the one with instance id
-  	#
+	## isInstanceAvailable
+	##
+	# input: class (the class of which an instance is to be searched e.g. ::Timestamp or ::Video)
+	# input: id (the id of the class instance we are looking for e.g.: timestamp7 or video2)
+	#
+	# returns:
+	#  - in case the instance is found the instance is returned (return $x)
+	#  - in case there are no matching instances found return code 0 is returned
+	#    this could also mean that there are instances available but not the one with instance id
+	#
 	:public object method isInstanceAvailable {class id} {
-	  	set instlist [$class info instances -closure]
-	  	if {[llength $instlist] > 0} {
+		set instlist [$class info instances -closure]
+		if {[llength $instlist] > 0} {
 			puts "(@[current method]) $class instances available"
 			foreach i $instlist {
 				puts "(@[current method]) [current] is looking for $class:$id comparing to [$i info class]:[$i id get]"
@@ -34,17 +34,17 @@ nx::Class create Helper {
 			puts "(@[current method]) video instances of $class not available"
 		    return 0
 		}
-	}	
+	}
 }
 
 
 #
-# Video 
+# Video
 #
 
-nx::Class create Video -mixins Helper {
-  	# TODO make the id not callable via DSL
-  	:property -accessor public {id empty}
+nx::Class create Video {
+	# TODO make the id not callable via DSL
+	:property -accessor public {id empty}
 	:property -accessor public {URL empty}
 	:property -accessor public {timestamp empty}
 	:property {timestampClass ::Timestamp}
@@ -68,22 +68,18 @@ nx::Class create Video -mixins Helper {
 	}
 
 	:method init {} {
-		# check if we already have a video instance with id
-		#set v [Helper isInstanceAvailable ::Video ${:id}]
-		#if {$v ne 0} {
-		#	puts "video instance ${:id} already instantiated: v:$v [$v id get] self:[self] [[self] id get]"	
-		#}
+		# handle -timestamp parameter
 		if {${:timestamp} ne "empty"} {
-		  	set ti [Helper isInstanceAvailable ${:timestampClass} ${:timestamp}]
-		  	puts "(@[current method]) ti: $ti"
+			set ti [Helper isInstanceAvailable ${:timestampClass} ${:timestamp}]
+			puts "(@[current method]) ti: $ti"
 			if {$ti ne 0} {
-			  	puts "(@[current method]) timestamp:[[self] timestamp get]"
+				puts "(@[current method]) timestamp:[[self] timestamp get]"
 				:addTimestamp -ts $ti
 			} else {
 				# case 1: timestamp not found, destroy video / but we don't always want to destroy it
 			#	puts "(@[current method]) video instances: [Video info instances -closure] is this self [self]"
 				dict set returnOptions customOptions "makeEmpty" "timestamp set empty"
-			  	dict set returnOptions customOptions "not found" "${:timestamp}"
+				dict set returnOptions customOptions "not found" "${:timestamp}"
 				dict set returnOptions customOptions "caller" [self]
 				return -code 5 -options $returnOptions "${:timestampClass}:${:timestamp} not found"
 			#	return -code 5 "${:timestampClass}:${:timestamp} not found"
@@ -94,7 +90,7 @@ nx::Class create Video -mixins Helper {
 
 	:method createTimestamp {args} {
 		#${:timestampClass} create [:]::[:autoname ${:prefix}] {*}$args
-	  	# puts "is [:] the same as [self]"
+		# puts "is [:] the same as [self]"
 		${:timestampClass} new -childof [current] {*}$args
 	}
 
@@ -111,7 +107,7 @@ nx::Class create Video -mixins Helper {
 	} {
 		# args should allow list of one to many items of only timestamps
 		# check if list objects are timestamps
-	  	# foreach timestamp in list createTimestamp
+		# foreach timestamp in list createTimestamp
 		foreach el $tslist {
 		  :addTimestamp -ts $el
 		}
@@ -123,38 +119,38 @@ nx::Class create Video -mixins Helper {
 #
 
 nx::Class create Timestamp {
-  	:property -accessor public {id empty}
+	:property -accessor public {id empty}
 	:property -accessor public {time:integer 0}
 	:property -accessor public {title empty}
 	:property -accessor public {video empty}
-	
+
 	:method init {} {
 		if {${:video} ne "empty"} {
-		  	set y [:isInstanceAvailable ::Video ${:video}]
-		  	if {$y ne 0} {
-			  	puts video:[[self] video get]
+			set y [Helper isInstanceAvailable ::Video ${:video}]
+			if {$y ne 0} {
+				puts video:[[self] video get]
 				$y :addTimestamp -ts [self]
 			} else {
-			  	puts "video ${:video} not found. Raising error"
+				puts "video ${:video} not found. Raising error"
 				return -code 5 -options $returnOptions "::Video:${:video} not found"
 			}
 		} elseif {${:video} eq "empty"} {
-		  	puts "A timestamp cannot be created without a video" 
+			puts "A timestamp cannot be created without a video"
 		}
 
 
 
 #		if {${:video} ne "empty"} {
-#		  	# check if video is available
-#		  	set success 0
+#			# check if video is available
+#			set success 0
 #			set vlist [Video info instances -closure]
 #			if {[llength $vlist] > 0} {
 #				foreach x $vlist {
 #					if {${:video} eq [$x id get]} {
-#					  	# create timestamp again within video container
+#						# create timestamp again within video container
 #						#${:video} createTimestamp -time ${:time} -title ${:title}
-#		    			puts "x:$x self:[[self] id get]"
-#					  	$x addTimestamp -ts [self]
+#						puts "x:$x self:[[self] id get]"
+#						$x addTimestamp -ts [self]
 #						set success 1
 #						break
 #					} else {
@@ -180,7 +176,7 @@ nx::Class create Timestamp {
 nx::Class create Module {
 	:property -accessor public {title}
 	:property -accessor public {structure}
-} 
+}
 
 namespace export Video Timestamp Module Helper
 }
